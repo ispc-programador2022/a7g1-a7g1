86b4fc4544100931edb1603404c93b2741698772
solhsolh <33585392+solhsolh@users.noreply.github.com>
2022-06-16 02:12:20 -0300

    ejercios3y4

--- A7G1.py
+++ A7G1.py
@@ -1,1 +1,12 @@
-#
+#A7 G1 HILGEMBERG MARIA SOL EJERCICIO 3Y4
+
+ 
+def retornar_producto(a,b):
+    producto=a * b
+    return producto
+
+
+def desguazar(p1, p2):
+  cociente = p1 // p2
+  resto = p1 % p2
+  return cociente,resto
