5924ff657ee18de4c02bdb3d89f9a9cb3594ec65
laurasg2020 <gutierrezlaurasoledad2020@gmail.com>
2022-06-19 19:22:29 -0300

    .

---
+++ suma.py
@@ -0,0 +1,5 @@
+# Suma dos numeros enteros cuales quiera ingresando su valores consecutivamente
+n1 = int(input("INGRESE UN NUMERO: "))
+n2 = int(input("INGRESE OTRO NUMERO: "))
+sum = n1 + n2
+print("EL RESULTADO ES: ", sum)
