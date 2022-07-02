1872e4e7f29ca58efdf38928a1e379d3e16ad879
KarimJuri <karim.dev.tech@gmail.com>
2022-07-01 21:59:15 -0300

    función que calcule devuelva el mínimo del vector obtenido en genrnd

---
+++ minimo_del_vector.py
@@ -0,0 +1,7 @@
+# función que calcule devuelva el mínimo del vector obtenido en genrnd
+def minimo():
+    mini = sorted(genrnd)
+    print("el minimo del vector obtenido en genrnd es: ", mini[0])
+minimo()
+
+ #aporte hecho por Marcelo Karim Juri Garay
