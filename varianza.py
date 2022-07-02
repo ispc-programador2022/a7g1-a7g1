21af55dd9805c1249f48d7ddad4799dd41f1d7bf
KarimJuri <karim.dev.tech@gmail.com>
2022-07-01 21:53:49 -0300

    función que calcule la varianza del vector obtenido en genrnd

---
+++ varianza.py
@@ -0,0 +1,10 @@
+#función que calcula la varianza del vector obtenido en genrnd
+
+def varianza(lstgen):
+    media = sum(lstgen) / len(lstgen)
+    var=0
+    for i in lstgen:
+        var += (i - media)**2
+    print("la varianza del vector obtenido en genrnd es: ", var / (len(lstgen) -1))
+    
+varianza()
